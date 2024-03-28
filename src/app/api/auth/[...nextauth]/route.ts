1ed29ea84d7b087fb69a6988a88e98d0ac856ba3
TechnologicNick <technologicnick@gmail.com>
2024-03-28 17:13:09 +0100

    Replace Discord authentication with Steam

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,21 @@
 import NextAuth from "next-auth";
+import SteamProvider from 'next-auth-steam'
+import { NextRequest } from "next/server";
+import { env } from "~/env";
 
 import { authOptions } from "~/server/auth";
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-const handler = NextAuth(authOptions);
+const handler = async (req: NextRequest, ctx: { params: { nextauth: string[] } }) => {
+  return NextAuth(req, ctx, {
+    ...authOptions,
+    providers: [
+      ...authOptions.providers,
+      SteamProvider(req, {
+        clientSecret: env.STEAM_API_KEY,
+        callbackUrl: `${env.NEXTAUTH_URL}/api/auth/callback`,
+      }),
+    ]
+  });
+}
+
 export { handler as GET, handler as POST };
