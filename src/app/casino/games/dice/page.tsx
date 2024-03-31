f616f82f705c941da50f7e8e4c31ffa356896bd3
TechnologicNick <technologicnick@gmail.com>
2024-03-31 17:06:42 +0200

    Add react-three-fiber and a die

--- src/app/casino/games/dice/page.tsx
+++ src/app/casino/games/dice/page.tsx
@@ -1,9 +1,25 @@
-import { Button } from "@nextui-org/button";
+"use client";
+
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import { Die } from "@/models/die";
 
 export default function Page() {
   return (
-    <div>
-      <Button>Click me</Button>
+    <div className="h-full w-full">
+      <Canvas>
+        <PerspectiveCamera
+          name="camera"
+          fov={40}
+          near={10}
+          far={1000}
+          position={[10, 0, 50]}
+        />
+        <OrbitControls />
+        <ambientLight intensity={0.1} />
+        <directionalLight color="white" position={[2, 4, 5]} />
+        <Die />
+      </Canvas>
     </div>
   );
 }
